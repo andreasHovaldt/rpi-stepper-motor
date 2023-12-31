78bc36a3236de273d911f5f8d0f3de9f199acebe
andreasHovaldt <andreas.hovaldt@gmail.com>
2023-12-31 03:44:41 +0100

    Rewritten code to be class oriented

--- motor_class.py
+++ motor_class.py
@@ -1,72 +1,82 @@
-#import RPi.GPIO as GPIO
+#!/usr/bin/python3
 from gpiozero import OutputDevice
 import time
 
-in1 = OutputDevice(16)
-in2 = OutputDevice(19)
-in3 = OutputDevice(20)
-in4 = OutputDevice(26)
-motor_pins = [in1,in2,in3,in4]
 
-# careful lowering this, at some point you run into the mechanical limitation o>
-step_sleep = 0.002
-
-#step_count = 4096 # 5.625*(1/64) per step, 4096 steps is 360 degrees
-
-#direction = False # True for clockwise, False for counter-clockwise
-
-# defining stepper motor sequence (found in documentation http://www.4tronix.co>
-step_sequence = [[1,0,0,1],
-                 [1,0,0,0],
-                 [1,1,0,0],
-                 [0,1,0,0],
-                 [0,1,1,0],
-                 [0,0,1,0],
-                 [0,0,1,1],
-                 [0,0,0,1]]
-motor_step_counter = 0
-
-def cleanup():
-    for pin in motor_pins:
-        pin.off()
+class StepperMotor():
+    def __init__(self, motor_pins: list, step_sleep = 0.002) -> None:
+        '''
+        Class for controlling a stepper motor
+        
+        Parameters:
+        - motor_pins (gpiozero.OutputDevice list): A list containing the 4 output pins to the stepper motor. Example: [in1, in2, in3, in4]
+        - step_sleep (int): The time, in seconds, to sleep between motor steps. Be careful lowering this too much, at some point you run into the mechanical limitation of how quick the motor can move. Default = 0.002
+        '''
+        
+        self.motor_pins = motor_pins
+        self.step_sleep = step_sleep
+        
+        # Defining stepper motor sequence (Found in documentation <http://www.4tronix.co.uk/arduino/Stepper-Motors.php>)
+        self.step_sequence = [[0,0,0,1],
+                              [0,0,1,1],
+                              [0,0,1,0],
+                              [0,1,1,0],
+                              [0,1,0,0],
+                              [1,1,0,0],
+                              [1,0,0,0],
+                              [1,0,0,1]]
+        self.motor_step_counter = 0 # Used to keep count of where the motor is located in the step_sequence
 
 
-def step_motor(step_count: int, direction: bool):
-    '''
-    Step motor the given number of steps in the given direction
-    
-    Parameters:
-    - step_count (int): Number of steps the motor should perform
-    - direction (bool): True for clockwise rotation, false for counter-clockwise rotation
-    '''
-    
-    global motor_pins, step_sleep, step_sequence, motor_step_counter
-    
-    try:
-        for _ in range(step_count):
-            for pin, value in zip(motor_pins, step_sequence[motor_step_counter]):
-                pin.value = value
-            if direction:
-                motor_step_counter = (motor_step_counter - 1) % 8
-            else:
-                motor_step_counter = (motor_step_counter + 1) % 8
-            time.sleep(step_sleep)
-            
-    except KeyboardInterrupt:
-        cleanup()
-        exit(1)
+    def stop(self) -> None:
+        ''' Helper function for turning all pins off'''
+        for pin in self.motor_pins:
+            pin.off()
+
+
+    def step_motor(self, step_count: int, direction: bool) -> None:
+        '''
+        Step motor the given number of steps in the given direction
+        
+        Parameters:
+        - step_count (int): Number of steps the motor should perform
+        - direction (bool): True for clockwise rotation, false for counter-clockwise rotation
+        '''
+        
+        try:
+            for _ in range(step_count):
+                for pin, value in zip(self.motor_pins, self.step_sequence[self.motor_step_counter]):
+                    pin.value = value
+                if direction:
+                    self.motor_step_counter = (self.motor_step_counter + 1) % 8
+                else:
+                    self.motor_step_counter = (self.motor_step_counter - 1) % 8
+                time.sleep(self.step_sleep)
+                
+        except KeyboardInterrupt:
+            self.stop()
+            exit(1)
 
 
 def main():
-    # motor_step_counter = 0
-    cleanup()
-
-    step_motor(int(4096/2), True)
-
-    cleanup()
-    exit( 0 )
-
-
+    # Define motor output pins
+    in1 = OutputDevice(16)
+    in2 = OutputDevice(19)
+    in3 = OutputDevice(20)
+    in4 = OutputDevice(26)
+    
+    # Initialize motor
+    motor = StepperMotor([in1, in2, in3, in4])
+    
+    # Run motor half a rotation 
+    motor.step_motor(int(4096/2), True)
+    time.sleep(1)
+    motor.step_motor(int(4096/2), False)
+    
+    
+    motor.stop()
+    print("Execution finished, exiting...")
+    exit()
 
 
 
